8afa8e2d01bf295a69d71575c4cd0d9c3293d11d
Maximo <maxovaroy@gmail.com>
2025-12-02 12:39:13 +0530

    Update profile.py

--- cogs/profile.py
+++ cogs/profile.py
@@ -11,22 +11,18 @@ class Profile(commands.Cog):
         if message.author.bot:
             return
 
-        uid = str(message.author.id)
-        db.add_message(uid)
+        await db.add_message(str(message.author.id))
 
     @commands.command()
     async def profile(self, ctx):
-        uid = str(ctx.author.id)
-        result = db.get_user(uid)
-
-        user_id, xp, lvl, messages, aura = result
+        user = await db.get_user(str(ctx.author.id))
+        user_id, xp, level, messages, aura = user
 
         embed = discord.Embed(
             title=f"{ctx.author.name}'s Profile",
-            description="Your Merlin stats",
             color=0x00ff99
         )
-        embed.add_field(name="Level", value=lvl)
+        embed.add_field(name="Level", value=level)
         embed.add_field(name="XP", value=xp)
         embed.add_field(name="Messages", value=messages)
         embed.add_field(name="Aura", value=aura)
